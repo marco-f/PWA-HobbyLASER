742b641c04ea2dd445bc15f8d8cb45620dc6a341
m <marco.ferlin.62@gmail.com>
2025-04-11 11:39:19 +0200

    Aggiunti nuovi file e modifiche

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+        if ('serviceWorker' in navigator) {
+                window.addEventListener('load', () => {
+                        navigator.serviceWorker.register('/service-worker.js')
+                                .then((registration) => {
+                                        console.log('Service Worker successfully registered:', registration)
+                                })
+                                .catch((error) => {
+                                        console.log('Service Worker Registration Failed:', error)
+                                })
+                })
+        }
+})
